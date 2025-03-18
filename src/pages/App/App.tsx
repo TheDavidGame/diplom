67472ddcbb2314af60282797fc5be53204aa573d
Vladislav <vlados.billi@gmail.com>
2025-03-18 18:27:59 +0300

    add Header

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MainPage from "../MainPage/MainPage";
+import UnderMainPage from "../UnderMainPage/UnderMainPage";
 
 function App() {
   return (
     <div className="App">
       <MainPage />
+      <UnderMainPage />
 
     </div>
   );
--- src/pages/UnderMainPage/UnderMainPage.tsx
+++ src/pages/UnderMainPage/UnderMainPage.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
-import MainPageStyles from './MainPage.module.scss';
+import UnderMainPageStyles from './UnderMainPage.module.scss';
 
 const UnderMainPage = () => {
-
     return (
-        <div className={MainPageStyles.wrapper}>
+        <div className={UnderMainPageStyles.wrapper}>
+            <div className={UnderMainPageStyles.header}>
+                <img
+                    src="/bckrndHeader.svg"
+                    alt="Header background"
+                    className={UnderMainPageStyles.background}
+                />
 
+                <div className={UnderMainPageStyles.burger}>
+                    <img src="/burgerHeader.svg" alt="Menu" />
+                </div>
+
+                <div className={UnderMainPageStyles.logo}>
+                    <img src="/logoHeader.svg" alt="Logo" />
+                </div>
+
+                <div className={UnderMainPageStyles.reservation}>
+                    <img src="/reservationHeader.svg" alt="Reservation" />
+                </div>
+            </div>
+
+            <div className={UnderMainPageStyles.underHeader}>
+            </div>
+
+            <div className={UnderMainPageStyles.scroller}>
+            </div>
+
+            <div className={UnderMainPageStyles.underScroller}>
+            </div>
         </div>
     )
 }
