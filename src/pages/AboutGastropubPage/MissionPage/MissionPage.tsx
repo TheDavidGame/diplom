3c08d693cd739f4934f1304c95d37e15475ccbfd
Vladislav <vlados.billi@gmail.com>
2025-05-11 14:27:59 +0300

    add stripes

--- src/pages/AboutGastropubPage/MissionPage/MissionPage.tsx
+++ src/pages/AboutGastropubPage/MissionPage/MissionPage.tsx
@@ -4,6 +4,16 @@ import MissionPageStyles from './MissionPage.module.scss';
 const MissionPage = () => {
     return (
         <div className={MissionPageStyles.wrapper}>
+            <div className={MissionPageStyles.stripes}>
+                <div className={MissionPageStyles.stripe} style={{ top: '250px', left: '-15%', transform: 'rotate(-12deg)' }}>
+                    СОБЫТИЕ ГАСТРОНОМ
+                </div>
+                <div className={MissionPageStyles.stripe} style={{ top: '300px', left: '-10%', transform: 'rotate(5deg)' }}>
+                    НОВОЕ ИСКУССТВО
+                </div>
+
+            </div>
+
             <div className={MissionPageStyles.main}>
                 <div className={MissionPageStyles.mainTitle}>
                     МИССИЯ
