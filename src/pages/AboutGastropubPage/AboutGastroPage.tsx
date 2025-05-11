eeb8feef31af0eed4a2e4fcab2fb32c59493fcc1
Vladislav <vlados.billi@gmail.com>
2025-05-11 15:41:33 +0300

    add UnderMissionPage

--- src/pages/AboutGastropubPage/AboutGastroPage.tsx
+++ src/pages/AboutGastropubPage/AboutGastroPage.tsx
@@ -2,19 +2,21 @@ import React, {useRef, useState} from 'react';
 import MainPage from "./MainPage/MainPage";
 import UnderHeader from "./UnderHeader/UnderHeader";
 import MissionPage from "./MissionPage/MissionPage";
+import UnderMissionPage from "./UnderMissionPage/UnderMissionPage";
 
 const AboutGastroPage = () => {
     const mainPageRef = useRef<HTMLDivElement>(null);
     const [isMainPageVisible, setIsMainPageVisible] = useState(true);
     return (
         <div>
-            <MainPage ref={mainPageRef} />
+            <MainPage ref={mainPageRef}/>
             <UnderHeader
                 mainPageRef={mainPageRef}
                 isMainPageVisible={isMainPageVisible}
                 setIsMainPageVisible={setIsMainPageVisible}
             />
-            <MissionPage />
+            <MissionPage/>
+            <UnderMissionPage/>
         </div>
     );
 };
