e9d3a0705d1b0a69a8c6135aaf2c92db52355ee2
Vladislav <vlados.billi@gmail.com>
2025-05-11 11:54:42 +0300

    change homePage

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,36 +1,8 @@
-import {useRef, useState} from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
-import MainPage from "../MainPage/MainPage";
-import UnderMainPage from "../UnderMainPage/UnderMainPage";
-import AboutUsPage from "../AboutUsPage/AboutUsPage";
-import EventsPage from "../EventsPage/EventsPage";
-import AwardsPage from "../AwardsPage/AwardsPage";
-import UpperFooterPage from "../UpperFooterPage/UpperFooterPage";
-import AppFooter from "../AppFooter/AppFooter";
 import MenuPage from "../MenuPage/MenuPage";
 import DoneReservationModalPage from "../ReservationModalPage/DoneReservationModalPage/DoneReservationModalPage";
-
-const HomePage = () => {
-    const mainPageRef = useRef<HTMLDivElement>(null);
-    const [isMainPageVisible, setIsMainPageVisible] = useState(true);
-
-    return (
-        <>
-            <MainPage ref={mainPageRef} />
-            <UnderMainPage
-                mainPageRef={mainPageRef}
-                isMainPageVisible={isMainPageVisible}
-                setIsMainPageVisible={setIsMainPageVisible}
-            />
-            <AboutUsPage />
-            <EventsPage />
-            <AwardsPage />
-            <UpperFooterPage />
-            <AppFooter />
-        </>
-    );
-};
+import HomePage from "../HomePage/HomePage";
 
 const App = () => {
     return (
